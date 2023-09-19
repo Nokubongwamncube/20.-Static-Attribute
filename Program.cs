0bcd7a008161fddefc142091508df358c9243333
Dell-User <Dell-User@DESKTOP-4LJVQAT>
2023-09-19 02:57:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20.Static_Attribute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            songs song1 = new songs("Holiday", "Green Day", 115);
+            songs song2 = new songs("Billy Jeans", "MJ", 200);
+            Console.WriteLine(song1.title);
+            Console.WriteLine(songs.songcount);
+
+            //frezze
+            Console.Read();
+
+
+
+        }
+    }
+}
---
+++ songs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20.Static_Attribute
+{
+    class songs
+    {
+        public string title;
+        public string artist;
+        public int duration;
+
+        public static int songcount; //declared a atatic attribute
+
+        public songs(string aTitle, string aArtist, int aDuration)
+        {
+            title = aTitle;
+            artist = aArtist;
+            duration = aDuration;
+
+            songcount++;
+
+
+        
+
+                
+
+        }
+
+
+
+
+   
+
+
+    }
+}
